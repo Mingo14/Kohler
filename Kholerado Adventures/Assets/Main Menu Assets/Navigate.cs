73e73c02909b96b3f0bdc79cb5da6baf983e6a19
ANDRES H CARRERA <andreshcar@live.com>
2019-02-24 11:23:27 -0700

    adding some structure to the application and made the switch to just changing cameras

---
+++ Kholerado Adventures/Assets/Main Menu Assets/Navigate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigate : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cameraToActivate;
+    [SerializeField]
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    private void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        mainCamera.enabled = false;
+        cameraToActivate.enabled = true;
+    }
+}
