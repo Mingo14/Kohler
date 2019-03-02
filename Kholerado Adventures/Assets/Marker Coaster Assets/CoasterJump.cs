fd76c856188c82f5d0a9c7b9d5eb4f6ddf13194f
ANDRES H CARRERA <andreshcar@live.com>
2019-03-02 16:03:16 -0700

    adding the first version of the first clicker game

---
+++ Kholerado Adventures/Assets/Marker Coaster Assets/CoasterJump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoasterJump : MonoBehaviour
+{
+    [SerializeField] GameObject m_coaster = null;
+    [SerializeField] [Range(1,10)] float m_jumpPower = 0.0f;
+
+    private Rigidbody m_rb = null;
+    private void Start()
+    {
+        m_rb = m_coaster.GetComponent<Rigidbody>();
+        
+    }
+    private void OnMouseDown()
+    {
+        m_rb.velocity += Vector3.up * m_jumpPower;
+    }
+}
